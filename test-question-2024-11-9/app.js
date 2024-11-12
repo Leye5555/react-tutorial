a0c60c5f3f949b540bb9b395b3e3e3d47ec79730
Olubummo Faith <59509795+Leye5555@users.noreply.github.com>
2024-11-12 20:25:15 +0100

    day 10-11 notes

--- test-question-2024-11-9/app.js
+++ test-question-2024-11-9/app.js
@@ -1,4 +1,5 @@
 function computeRowElements(rowIndex) {
+  console.time("process-compute");
   let rowArr = [];
   let tempArr = [];
   for (let i = 0; i <= rowIndex; i++) {
@@ -14,9 +15,37 @@ function computeRowElements(rowIndex) {
     }
   }
   console.log(rowArr);
+  console.timeEnd("process-compute");
   return rowArr;
 }
 
 computeRowElements(0);
 computeRowElements(3);
 computeRowElements(5);
+
+function computeRowElementsRefactored(rowIndex) {
+  console.time("process-test");
+  let prev = 1;
+  let rowArr = [prev];
+  for (let i = 1; i <= rowIndex; i++) {
+    let item = prev * ((rowIndex - i + 1) / i);
+    prev = item;
+    rowArr.push(item);
+  }
+  console.log(rowArr);
+  console.timeEnd("process-test");
+  return rowArr;
+}
+
+computeRowElementsRefactored(10);
+computeRowElementsRefactored(6);
+computeRowElementsRefactored(3);
+
+/**
+ *
+ * [] [] [] []
+ * 3! = 3 * 2 * 1
+ * 4! = 4 * 3 * 2 * 1
+ *
+ * 3! /
+ */
